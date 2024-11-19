a7c92f2a27561e522be94c6ee7e3f784e6a622ba
Thanveer KP <thanveerkp99@gmail.com>
2024-11-19 15:54:29 +0530

    Sorted

--- Array/array4.c
+++ Array/array4.c
@@ -1,26 +1,31 @@
-#include<stdio.h>
-void main(){
-    int values[100],limit,i,j,temp;
+#include <stdio.h>
+void main()
+{
+    int values[100], limit, i, j, temp;
     printf("Enter a Limit : ");
-    scanf("%d",&limit);
+    scanf("%d", &limit);
     printf("Enter Values : \n");
-    for(i=0;i<limit;i++){
-        scanf("%d",&values[i]);
+    for (i = 0; i < limit; i++)
+    {
+        scanf("%d", &values[i]);
     }
 
-    for(i=0;i<limit;i++){
-        for(j=i+1;j<limit;j++){
-            if(values[i]>values[j]){
-                temp=values[i];
-                values[i]=values[j];
-                values[j]=temp;
-
+    for (i = 0; i < limit; i++)
+    {
+        for (j = i + 1; j < limit; j++)
+        {
+            if (values[i] > values[j])
+            {
+                temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
             }
         }
     }
 
     printf("Sorted values : ");
-    for(i=0;i<limit;i++){
-        printf("%d\t",values[i]);
+    for (i = 0; i < limit; i++)
+    {
+        printf("%d\t", values[i]);
     }
 }
