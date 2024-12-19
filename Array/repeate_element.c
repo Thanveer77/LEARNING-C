3ac7b24e6ddeaf5675569374853347b2e1ab70cf
Thanveer KP <thanveerkp99@gmail.com>
2024-12-19 11:39:20 +0530

    most repeate element

--- Array/repeate_element.c
+++ Array/repeate_element.c
@@ -2,25 +2,26 @@
 
 int main()
 {
-    int a[10]={3,2,3,3,3,4,3,4,3,5};
-    int i,j,count,maxelement=0,n=10;
-    
-    for(i=0;i<n;i++){
-        count=1;
-        for(j=i+1;j<n;j++){
-            if(a[i]==a[j]){
+    int a[10] = {3, 2, 3, 3, 3, 4, 3, 4, 3, 5};
+    int i, j, count, maxelement = 0, n = 10;
+
+    for (i = 0; i < n; i++)
+    {
+        count = 1;
+        for (j = i + 1; j < n; j++)
+        {
+            if (a[i] == a[j])
+            {
                 count++;
-            
-             if(count>maxelement){
-                maxelement=a[j];
+
+                if (count > maxelement)
+                {
+                    maxelement = a[j];
+                }
             }
-            }
-            
-            
         }
-        
     }
-    printf("%d",maxelement);
+    printf("%d", maxelement);
 
     return 0;
 }
